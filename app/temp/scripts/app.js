4f60ccb806b35ff6c2d63815d719ef222302a9f4
Simone Westphal <contact@swestphal.net>
2018-02-10 12:43:21 +0100

    chore: refactoring app.js code

--- app/temp/scripts/app.js
+++ app/temp/scripts/app.js
@@ -70,267 +70,243 @@
 "use strict";
 
 
-var _Game = __webpack_require__(1);
+var memory = function () {
 
-var _Game2 = _interopRequireDefault(_Game);
+    // $(".site-header__menu-icon").click(function () {
+    //     console.log("right icon was clicked");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+    var fieldSize = 16;
+    var cardClickCounter = 1;
+    var showCardClickCounter = document.getElementById('card-click-counter');
+    var showCardRating = document.getElementById('card-star-rating');
+    var showCardTimer = document.getElementById('card-timer');
+    var showGameField = document.getElementById('field-table');
 
-new _Game2.default();
+    var cardClick = document.getElementById('field');
+    var openCards = [];
+    var strg = "";
+    var oldId;
+    var refreshIntervalId;
+    var firstClickTime;
+    var rating;
+    var starRating;
 
-/***/ }),
-/* 1 */
-/***/ (function(module, exports, __webpack_require__) {
+    var cardArr = [];
 
-"use strict";
+    generateCards();
 
+    play();
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Game = function () {
-    function Game() {
-        _classCallCheck(this, Game);
-
-        // $(".site-header__menu-icon").click(function () {
-        //     console.log("right icon was clicked");
-
-        this.fieldSize = 16;
-        this.cardClickCounter = 1;
-        this.showCardClickCounter = document.getElementById('card-click-counter');
-        this.showCardRating = document.getElementById('card-star-rating');
-        this.showCardTimer = document.getElementById('card-timer');
-        this.showGameField = document.getElementById('field-table');
-
-        this.cardClick = document.getElementById('field');
-        this.openCards = [];
-        this.strg = "";
-
-        this.refreshIntervalId;
-        this.firstClickTime;
-        this.arr = [];
-        this.generateCards();
-        this.events();
+    function timerStop() {
+        clearInterval(refreshIntervalId);
     }
 
-    _createClass(Game, [{
-        key: 'timerStop',
-        value: function timerStop() {
-            clearInterval(this.refreshIntervalId);
+    function timerStart() {
+        //remove eventlistener after first call of timerStart
+        cardClick.removeEventListener('click', timerStart);
+
+        // save time of first click
+        firstClickTime = new Date();
+
+        // update timer every second
+        refreshIntervalId = setInterval(timerUpdate.bind(this), 1000);
+    }
+
+    function timeFormat(value) {
+        // convert value to string
+        var value = value.toString();
+
+        // if only one digit length put 0 before
+        if (value.length <= 1) {
+            return "0" + value;
+        };
+
+        //else go back like it was
+        return value;
+    }
+
+    function timerUpdate() {
+        var currentTime = new Date();
+        var diffTime = currentTime.getTime() - firstClickTime.getTime();
+
+        var diffHours = Math.floor(diffTime / (1000 * 60 * 60));
+        diffTime -= diffHours * (1000 * 60 * 60);
+
+        var diffMin = Math.floor(diffTime / (1000 * 60));
+        diffTime -= diffMin * 1000 * 60;
+
+        var diffSec = Math.floor(diffTime / 1000);
+        if (diffSec == 10) timerStop();
+        showCardTimer.innerText = timeFormat(diffHours) + ":" + timeFormat(diffMin) + ":" + timeFormat(diffSec);
+    }
+
+    function play() {
+
+        //timerStart = timerStart.bind(this);
+        cardClick.addEventListener('click', timerStart);
+
+        cardClick.addEventListener('click', function () {
+            newCardClick();
+        });
+    }
+
+    function newCardClick() {
+        // verify, that user clicked on td element
+        if (event.target.nodeName.toLowerCase() == 'td') {
+            var id = event.target.dataset.id;
+
+            if (cardArr[id].isOpen == false && !cardArr[id].isOpen && oldId != id) {
+                console.log("go to check");
+                // increment the move-counter of clicks
+                // update move-counter on frontend
+                showCardClickCounter.innerText = cardClickCounter++;
+
+                checkRating();
+
+                checkCardClickChoice();
+            }
+            oldId = id;
         }
-    }, {
-        key: 'timerStart',
-        value: function timerStart() {
-            //remove eventlistener after first call of timerStart
-            this.cardClick.removeEventListener('click', this.timerStart);
+    }
 
-            // save time of first click
-            this.firstClickTime = new Date();
+    function generateCards() {
 
-            // update timer every second
-            this.refreshIntervalId = setInterval(this.timerUpdate.bind(this), 1000);
-        }
-    }, {
-        key: 'timeFormat',
-        value: function timeFormat(value) {
-            // convert value to string
-            var value = value.toString();
-
-            // if only one digit length put 0 before
-            if (value.length <= 1) {
-                return "0" + value;
+        for (var i = 0; i < fieldSize / 2; i++) {
+            var card1 = {
+                matchingPair: i,
+                isOpen: false,
+                isMatching: false,
+                content: i + 1
             };
-
-            //else go back like it was
-            return value;
+            var card2 = {
+                matchingPair: i,
+                isOpen: false,
+                isMatching: false,
+                content: i + fieldSize
+            };
+            cardArr.push(card1);
+            cardArr.push(card2);
         }
-    }, {
-        key: 'timerUpdate',
-        value: function timerUpdate() {
-            var currentTime = new Date();
-            var diffTime = currentTime.getTime() - this.firstClickTime.getTime();
+        shuffleCards();
+    }
 
-            var diffHours = Math.floor(diffTime / (1000 * 60 * 60));
-            diffTime -= diffHours * (1000 * 60 * 60);
+    function shuffleCards() {
+        // fisher-yates shuffle
+        var counter = cardArr.length;
 
-            var diffMin = Math.floor(diffTime / (1000 * 60));
-            diffTime -= diffMin * 1000 * 60;
+        // While there are elements in the arr
+        while (counter > 0) {
+            // pick a random index
+            var index = Math.floor(Math.random() * counter);
 
-            var diffSec = Math.floor(diffTime / 1000);
-            if (diffSec == 10) this.timerStop();
-            this.showCardTimer.innerText = this.timeFormat(diffHours) + ":" + this.timeFormat(diffMin) + ":" + this.timeFormat(diffSec);
+            // decrease counter by 1
+            counter--;
+
+            // swap the last element with it
+            var temp = cardArr[counter];
+            cardArr[counter] = cardArr[index];
+            // set position in the random order
+            cardArr[counter]['position'] = counter;
+
+            cardArr[index] = temp;
         }
-    }, {
-        key: 'events',
-        value: function events() {
 
-            this.timerStart = this.timerStart.bind(this);
-            this.cardClick.addEventListener('click', this.timerStart);
+        showShuffledCards();
+    }
 
-            this.newCardClick = this.newCardClick.bind(this);
-            this.cardClick.addEventListener('click', this.newCardClick);
+    function showShuffledCards() {
+        for (var row = 0; row < Math.sqrt(fieldSize); row++) {
+            var nodeRow = document.createElement("tr");
+
+            for (var col = 0; col < Math.sqrt(fieldSize); col++) {
+                var nodeCol = document.createElement("td");
+                var itemId = Math.sqrt(fieldSize) * row + col;
+                var content = document.createTextNode(cardArr[itemId]['matchingPair']);
+                nodeCol.appendChild(content);
+                nodeCol.dataset.id = itemId;
+                nodeRow.appendChild(nodeCol);
+            }
+            showGameField.appendChild(nodeRow);
         }
-    }, {
-        key: 'newCardClick',
-        value: function newCardClick(event) {
+        return;
+    }
 
-            // verify, that user clicked on td element
-            if (event.target.nodeName.toLowerCase() == 'td') {
+    function generateRatingStars(num) {
+        var stars = "";
+        for (var i = 0; i < num; i++) {
+            stars += "*";
+        }
+        return stars;
+    }
 
-                if (event.target.className != 'open') {
-                    // increment the move-counter of clicks
-                    // update move-counter on frontend
-                    this.showCardClickCounter.innerText = this.cardClickCounter++;
+    function checkRating() {
+        rating = (cardClickCounter - 1) / fieldSize;
+        switch (true) {
+            case rating <= 4.25:
+                starRating = 3;
 
-                    this.checkRating();
+                break;
+            case rating <= 5.75:
+                starRating = 2;
 
-                    this.checkCardClickChoice(event);
+                break;
+            case rating <= 7.5:
+                starRating = 1;
+
+                break;
+            default:
+                starRating = 0;
+        }
+
+        showCardRating.innerText = generateRatingStars(starRating);
+    }
+
+    function checkCardClickChoice() {
+
+        var openCard = event.target.dataset.id;
+        openCards.push(openCard);
+        cardArr[openCard].isOpen = true;
+
+        if (openCards.length <= 1) {
+            event.target.classList.add("open");
+        }
+
+        if (openCards.length == 2) {
+
+            event.target.classList.add("open");
+
+            var cardOneId = openCards[0];
+            cardArr[cardOneId].isOpen = true;
+
+            var cardTwoId = openCards[1];
+            cardArr[cardTwoId].isOpen = true;
+
+            openCards = [];
+            if (cardArr[cardOneId].matchingPair == cardArr[cardTwoId].matchingPair) {
+
+                var matchingPair = document.querySelectorAll(".open");
+                cardArr[cardOneId].isMatching = true;
+                cardArr[cardTwoId].isMatching = true;
+                for (var i = 0; i < matchingPair.length; i++) {
+
+                    console.log(cardArr);
+                    matchingPair[i].classList.remove("open");
+                    matchingPair[i].classList.add("matching");
                 }
-            }
-        }
-    }, {
-        key: 'generateCards',
-        value: function generateCards() {
-            var cardArr = [];
-            for (var i = 0; i < this.fieldSize / 2; i++) {
-                var card1 = {
-                    matchingPair: i,
-                    isVisible: false,
-                    content: i + 1
-                };
-                var card2 = {
-                    matchingPair: i,
-                    isVisible: false,
-                    content: i + this.fieldSize
-                };
-                cardArr.push(card1);
-                cardArr.push(card2);
-            }
-            this.shuffleCards(cardArr);
-        }
-    }, {
-        key: 'shuffleCards',
-        value: function shuffleCards(arr) {
-            // fisher-yates shuffle
-            var counter = arr.length;
+            } else {
+                cardArr[cardOneId].isOpen = false;
+                cardArr[cardTwoId].isOpen = false;
 
-            // While there are elements in the arr
-            while (counter > 1) {
-                // pick a random index
-                var index = Math.floor(Math.random() * counter);
-
-                // decrease counter by 1
-                counter--;
-
-                // swap the last element with it
-                var temp = arr[counter];
-                arr[counter] = arr[index];
-                // set position in the random order
-                arr[counter]['position'] = counter;
-
-                arr[index] = temp;
-            }
-            this.arr = arr;
-            this.showShuffledCards();
-        }
-    }, {
-        key: 'showShuffledCards',
-        value: function showShuffledCards() {
-            for (var row = 0; row < Math.sqrt(this.fieldSize); row++) {
-                var nodeRow = document.createElement("tr");
-
-                for (var col = 0; col < Math.sqrt(this.fieldSize); col++) {
-                    var nodeCol = document.createElement("td");
-                    var itemId = Math.sqrt(this.fieldSize) * row + col;
-                    var content = document.createTextNode(this.arr[itemId]['matchingPair']);
-                    nodeCol.appendChild(content);
-                    nodeCol.dataset.id = itemId;
-                    nodeRow.appendChild(nodeCol);
-                }
-                this.showGameField.appendChild(nodeRow);
-            }
-        }
-    }, {
-        key: 'generateRatingStars',
-        value: function generateRatingStars(num) {
-            var stars = "";
-            for (var i = 0; i < num; i++) {
-                stars += "*";
-            }
-            return stars;
-        }
-    }, {
-        key: 'checkRating',
-        value: function checkRating() {
-            this.rating = (this.cardClickCounter - 1) / this.fieldSize;
-            switch (true) {
-                case this.rating <= 4.25:
-                    this.starRating = 3;
-
-                    break;
-                case this.rating <= 5.75:
-                    this.starRating = 2;
-
-                    break;
-                case this.rating <= 7.5:
-                    this.starRating = 1;
-
-                    break;
-                default:
-                    this.starRating = 0;
-            }
-
-            this.showCardRating.innerText = this.generateRatingStars(this.starRating);
-        }
-    }, {
-        key: 'checkCardClickChoice',
-        value: function checkCardClickChoice(event) {
-            var openCard = event.target.dataset.id;
-
-            this.openCards.push(openCard);
-
-            if (this.openCards.length <= 1) {
-                event.target.classList.add("open");
-            }
-
-            if (this.openCards.length == 2) {
-
-                event.target.classList.add("open");
-
-                var cardOneId = this.openCards[0];
-                var cardTwoId = this.openCards[1];
-                this.openCards = [];
-
-                if (this.arr[cardOneId].matchingPair == this.arr[cardTwoId].matchingPair) {
-
-                    var matchingPair = document.querySelectorAll(".open");
-
-                    for (var i = 0; i < matchingPair.length; i++) {
-                        matchingPair[i].classList.remove("open");
-                        matchingPair[i].classList.add("matching");
+                setTimeout(function () {
+                    var pairs = document.querySelectorAll(".open");
+                    for (var i = 0; i < pairs.length; i++) {
+                        pairs[i].classList.remove("open");
                     }
-                } else {
-                    setTimeout(function () {
-                        var pairs = document.querySelectorAll(".open");
-
-                        for (var i = 0; i < pairs.length; i++) {
-                            pairs[i].classList.remove("open");
-                        }
-                    }, 1000);
-                }
+                }, 1000);
             }
         }
-    }]);
-
-    return Game;
+    }
 }();
-
-exports.default = Game;
 
 /***/ })
 /******/ ]);
