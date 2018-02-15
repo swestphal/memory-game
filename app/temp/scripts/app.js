d97b5fdcc019dbd18e682a0b5519936747d3f4bc
Simone Westphal <contact@swestphal.net>
2018-02-15 08:31:33 +0100

    feat: close modal & level hint on restart

--- app/temp/scripts/app.js
+++ app/temp/scripts/app.js
@@ -138,6 +138,8 @@ var memory = function () {
         while (myNode.firstChild) {
             myNode.removeChild(myNode.firstChild);
         }
+        changeLevelDisableHint();
+        modalFadeOutOpened();
 
         addHitlistToDom();
 
@@ -162,28 +164,30 @@ var memory = function () {
     }
 
     function getFromLocalStorage() {
-        return JSON.parse(localStorage.getItem("hitlist"));
+        if (isLocalStorageNameSupported()) return JSON.parse(localStorage.getItem("hitlist"));
     }
 
     function writeToLocalStore(obj) {
-        return localStorage.setItem("hitlist", JSON.stringify(obj));
+        if (isLocalStorageNameSupported()) return localStorage.setItem("hitlist", JSON.stringify(obj));
     }
 
     function pushToLocalStorage(newObj) {
-        // var newData = ({
-        //   fieldSize: fieldSize,
-        // moves: cardClickCounter,
-        //    minutes: diffMin,
-        //  seconds: diffSec
-        // });
+        if (isLocalStorageNameSupported()) {
+            // var newData = ({
+            //   fieldSize: fieldSize,
+            // moves: cardClickCounter,
+            //    minutes: diffMin,
+            //  seconds: diffSec
+            // });
 
-        var oldHitlist = getFromLocalStorage();
+            var oldHitlist = getFromLocalStorage();
 
-        if (oldHitlist == null) {
-            oldHitlist = [newObj];
-        } else oldHitlist.push(newObj);
-
-        writeToLocalStore(oldHitlist);
+            if (oldHitlist == null) {
+                oldHitlist = [newObj];
+            } else oldHitlist.push(newObj);
+            if (oldHitlist.length > 6) oldHitlist.shift();
+            writeToLocalStore(oldHitlist);
+        }
     }
 
     function removeNode(node) {
@@ -196,22 +200,28 @@ var memory = function () {
 
         removeNode(showHitlist);
 
-        var myFragment = document.createDocumentFragment();
-
-        var nodeUl = document.createElement("ul");
-
         var localHitlist = getFromLocalStorage();
-
         if (localHitlist) {
-            for (var i = 0; i < (localHitlist.length > 6 ? 6 : localHitlist.length); i++) {
-                var nodeLi = document.createElement("li");
-                var content = i + 1 + ")" + localHitlist[i].fieldSize + "fields with " + localHitlist[i].moves + " moves in " + localHitlist[i].time;
-                nodeLi.innerText = content;
-                myFragment.appendChild(nodeLi);
+
+            var myFragment = document.createDocumentFragment();
+
+            var nodeUl = document.createElement("ul");
+
+            if (localHitlist) {
+                for (var i = localHitlist.length > 6 ? 6 : localHitlist.length; i > 0; i--) {
+                    var nodeLi = document.createElement("li");
+                    var content = i + 1 + ")" + localHitlist[i].fieldSize + "fields with " + localHitlist[i].moves + " moves in " + localHitlist[i].time;
+                    nodeLi.innerText = content;
+                    myFragment.appendChild(nodeLi);
+                }
             }
+            nodeUl.appendChild(myFragment);
+            showHitlist.appendChild(nodeUl);
+        } else {
+            var nodeP = document.createElement("p");
+            nodeP.innerText = "Sorry, but there are no games saved.";
+            showHitlist.appendChild(nodeP);
         }
-        nodeUl.appendChild(myFragment);
-        showHitlist.appendChild(nodeUl);
     }
 
     function modalFadeIn(containerId) {
@@ -250,6 +260,7 @@ var memory = function () {
     function timerRestart() {
         clearInterval(refreshIntervalId);
         showCardTimer.innerText = "00:00:00";
+        cardClick.addEventListener('click', timerStart);
     }
 
     function timerStart() {
@@ -270,12 +281,17 @@ var memory = function () {
         return true;
     }
 
+    function changeLevelDisableHint() {
+        var hint = document.getElementById('game-level-hint');
+        hint.classList.remove('show');
+    }
+
     function changeLevelHint() {
         var hint = document.getElementById('game-level-hint');
         hint.classList.add('show');
         setTimeout(function () {
             hint.classList.remove('show');
-        }, 5000);
+        }, 4000);
     }
 
     function changeLevel() {
@@ -446,8 +462,6 @@ var memory = function () {
             }
             showGameField.appendChild(nodeRow);
         }
-
-        return;
     }
 
     function generateRatingStars(num) {
@@ -501,6 +515,8 @@ var memory = function () {
         };
         timeContent += diffSec + " seconds.";
 
+        modalMoves.innerText = cardClickCounter;
+        modalTime.innerText = timeContent;
         modalTime.innerText = timeContent;
 
         var newData = {
@@ -510,9 +526,7 @@ var memory = function () {
         };
 
         pushToLocalStorage(newData);
-
-        modalMoves.innerText = cardClickCounter;
-        modalTime.innerText = timeContent;
+        addHitlistToDom();
     }
 
     function flipCard(element, id) {
