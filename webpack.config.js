405e51e64b8b93fad4c31d0e6b612183d4ed4966
Simone Westphal <contact@swestphal.net>
2018-02-24 13:09:04 +0100

    fix: not cardclick but moves are counted

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,18 @@ var path = require('path');
 
 module.exports = {
     entry: "./app/assets/scripts/app.js",
-    output:
-        {
-            path: path.resolve(__dirname, "./app/temp/scripts"),
-            filename: 'app.js'
-        },
+    output: {
+        path: path.resolve(__dirname, "./app/temp/scripts"),
+        filename: 'app.js'
+    },
     module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                },
-                test: /\.js$/,
-                exclude: /\node_modules/
-            }
-        ]
+        loaders: [{
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            },
+            test: /\.js$/,
+            exclude: /\node_modules/
+        }]
     }
 };
